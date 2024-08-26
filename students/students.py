e309fe8c34289b9baf01eab71747dbdd4a165c9f
Dmitriy <SherDS@mail.ru>
2024-08-27 01:47:47 +0300

    Добавил файл students.py для создания класса - ученики

---
+++ students/students.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+class Student:
+
+    def __init__(self):
+        self.surname = None
+        self.name = None
+        self.surname_2 = None
+        self.age = None
+        self.group = None
+        self.avg_ball = None
+        self.note = None
+
